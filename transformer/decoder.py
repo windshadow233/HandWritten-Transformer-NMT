014c5eee41d98aab74792af2b97dcca6e6020e70
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-12-04 15:24:19 +0800

    瞎写一个beam_search

--- transformer/decoder.py
+++ transformer/decoder.py
@@ -34,7 +34,7 @@ class Decoder(nn.Module):
         self.vocab_size = config.vocab_size
         self.num_hidden_layers = config.num_hidden_layers
         self.embedding_dim = config.embedding_dim
-        self.max_len = config.max_seq_len
+        self.max_seq_len = config.max_seq_len
 
         self.word_embedding = Embedding(config)
         self.layers = nn.ModuleList([DecodeLayer(config) for _ in range(self.num_hidden_layers)])
@@ -45,23 +45,25 @@ class Decoder(nn.Module):
         # sos = padded_target.new_ones(size=(batch_size, 1)) * self.sos_idx
         # eos = padded_target.new_ones(size=(batch_size, 1)) * self.eos_idx
         # return torch.cat([sos, padded_target], dim=1), torch.cat([padded_target, eos], dim=1)
-        ys_in_pad = padded_target.clone()
-        ys_in_pad[ys_in_pad == self.eos_idx] = 0
-        ys_in_pad = ys_in_pad[:, :-1]
-        ys_out_pad = padded_target[:, 1:]
-        return ys_in_pad, ys_out_pad
+        input_data = padded_target.clone()
+        input_data[input_data == self.eos_idx] = 0
+        input_data = input_data[:, :-1]
+        target = padded_target[:, 1:]
+        return input_data, target
 
-    def forward(self, padded_target, padded_src, encoder_output):
-        ys_in_pad, ys_out_pad = self.pre_process(padded_target)
-        pad_mask = get_pad_mask(ys_in_pad, self.pad_idx)
-        self_attn_mask_subseq = get_subsequent_mask(ys_in_pad)
-        self_attn_mask = get_attention_mask(ys_in_pad, ys_in_pad, pad_idx=self.pad_idx)
+    def forward(self, padded_target, padded_src, encoder_output, softmax_at_end=False):
+        input_data, target = self.pre_process(padded_target)
+        pad_mask = get_pad_mask(input_data, self.pad_idx)
+        self_attn_mask_subseq = get_subsequent_mask(input_data)
+        self_attn_mask = get_attention_mask(input_data, input_data, pad_idx=self.pad_idx)
         self_attn_mask = ((self_attn_mask + self_attn_mask_subseq) > 0).float()
-        dec_enc_attn_mask = get_attention_mask(padded_src, ys_in_pad, pad_idx=self.pad_idx)
+        dec_enc_attn_mask = get_attention_mask(padded_src, input_data, pad_idx=self.pad_idx)
 
-        decode_output = self.word_embedding(ys_in_pad)
+        decode_output = self.word_embedding(input_data)
         for layer in self.layers:
             decode_output = layer(decode_output, encoder_output, pad_mask, self_attn_mask, dec_enc_attn_mask)
         decode_output = self.tgt_word_prj(decode_output)
-        return decode_output, ys_out_pad
+        if softmax_at_end:
+            decode_output = decode_output.softmax(-1)
+        return decode_output, target
 
--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from model_base import Config
 from encoder import Encoder
@@ -9,8 +10,53 @@ class Transformer(nn.Module):
         super(Transformer, self).__init__()
         self.encoder = Encoder(config)
         self.decoder = Decoder(config)
+        self.pad_idx = config.pad_idx
+        self.sos_idx = config.sos_idx
+        self.eos_idx = config.eos_idx
+        self.max_seq_len = config.max_seq_len
 
-    def forward(self, padded_input, padded_target):
-        encoder_output, _ = self.encoder(padded_input)
-        pred, gold = self.decoder(padded_target, padded_input, encoder_output)
-        return pred, gold
+    def forward(self, padded_src, padded_target):
+        encoder_output, _ = self.encoder(padded_src)
+        pred_logits, target = self.decoder(padded_target, padded_src, encoder_output)
+        return pred_logits, target
+
+    def predict_next(self, current_pred_sens, src_seq, encoder_output, beam_size=5):
+        """
+        :param current_pred_sens: 当前预测到的几个概率最大的序列(一个列表,每个元素为(current_len,))
+        :param src_seq: 源句子
+        :param encoder_output: encoder层编码结果
+        :param beam_size: 搜索空间
+        :return: 概率最大的beam_size个下一个预测序列
+        """
+        next_possible_sentences = []
+        for sen, score in current_pred_sens:
+            # sen:(1, current_len)
+            # score:(1,)
+            if sen.squeeze(0)[-1].item() == self.eos_idx:
+                next_possible_sentences.append((sen, score.item()))
+                continue
+            sen = torch.cat([sen, sen.new_full((1, 1), self.eos_idx)], 1)
+            pred_prob = self.decoder(sen, src_seq, encoder_output, softmax_at_end=True)[0]  # (1, current_len - 1, vocab_size)
+            max_prob, max_prob_word = pred_prob.sort(-1, descending=True)  # (1, current_len - 1, vocab_size)
+            max_prob, max_prob_word = max_prob[0, -1, :beam_size], max_prob_word[0, -1, :beam_size]  # (beam_size,)
+            next_possible_sentences.extend([(torch.cat([sen[:, :-1], word[None, None]], 1), (score + prob).item()) for word, prob in zip(max_prob_word, max_prob)])
+        next_possible_sentences.sort(key=lambda x: x[1], reverse=True)
+        return next_possible_sentences[:beam_size]
+
+    def beam_search(self, input_seq: torch.Tensor, max_len=100, beam_size=5):
+        """
+        对一个句子进行解码
+        :param input_seq: (L,)
+        :param max_len: 最大句长
+        :param beam_size: 单步搜索空间
+        :return:
+        """
+        input_seq = input_seq[None]  # (1, L)
+        encoder_output = self.encoder(input_seq)[0]  # (1, L, D)
+        sos = input_seq.new_full(size=(1, 1), fill_value=self.sos_idx)
+        pred = [(torch.cat([sos], dim=1), input_seq.new_zeros(size=(1, 1), dtype=torch.float32))]
+        for _ in range(max_len):
+            pred = self.predict_next(pred, input_seq, encoder_output, beam_size)
+        return pred[0]
+
+
