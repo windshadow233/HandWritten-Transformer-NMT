dbb7df2e6ff863367cf7dfc0e16d6ad4b1aac1fe
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-12-05 14:25:09 +0800

    Update transformer.py

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -33,13 +33,13 @@ class Transformer(nn.Module):
             # sen:(1, current_len)
             # score:(1,)
             if sen.squeeze(0)[-1].item() == self.eos_idx:
-                next_possible_sentences.append((sen, score.item()))
+                next_possible_sentences.append((sen, score))
                 continue
             sen = torch.cat([sen, sen.new_full((1, 1), self.eos_idx)], 1)
             pred_prob = self.decoder(sen, src_seq, encoder_output, softmax_at_end=True)[0]  # (1, current_len - 1, vocab_size)
             max_prob, max_prob_word = pred_prob.sort(-1, descending=True)  # (1, current_len - 1, vocab_size)
             max_prob, max_prob_word = max_prob[0, -1, :beam_size], max_prob_word[0, -1, :beam_size]  # (beam_size,)
-            next_possible_sentences.extend([(torch.cat([sen[:, :-1], word[None, None]], 1), (score + prob).item()) for word, prob in zip(max_prob_word, max_prob)])
+            next_possible_sentences.extend([(torch.cat([sen[:, :-1], word[None, None]], 1), score + prob.item()) for word, prob in zip(max_prob_word, max_prob)])
         next_possible_sentences.sort(key=lambda x: x[1], reverse=True)
         return next_possible_sentences[:beam_size]
 
@@ -54,7 +54,7 @@ class Transformer(nn.Module):
         input_seq = input_seq[None]  # (1, L)
         encoder_output = self.encoder(input_seq)[0]  # (1, L, D)
         sos = input_seq.new_full(size=(1, 1), fill_value=self.sos_idx)
-        pred = [(torch.cat([sos], dim=1), input_seq.new_zeros(size=(1,), dtype=torch.float32))]
+        pred = [(torch.cat([sos], dim=1), 0)]
         for _ in range(max_len):
             pred = self.predict_next(pred, input_seq, encoder_output, beam_size)
         return pred[0]
