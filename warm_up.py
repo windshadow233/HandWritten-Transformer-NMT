c5b397563cf2dc04ee9beeb0d88e80799c729e01
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-07 01:08:39 +0800

    Update warm_up.py

--- warm_up.py
+++ warm_up.py
@@ -2,14 +2,22 @@ from config import embedding_dim
 
 
 class WarmUpLr(object):
-    def __init__(self, optimizer, warmup_step):
+    def __init__(self, optimizer, init_lr, warmup_step):
         self.optimizer = optimizer
+        self.init_lr = init_lr
         self.warmup_step = warmup_step
         self.step_num = 0
 
+    def load_state_dict(self, d):
+        self.optimizer.load_state_dict(d)
+
+    def state_dict(self):
+        return self.optimizer.state_dict()
+
     def update_lr(self):
-        new_lr = embedding_dim ** -0.5 * min(self.step_num ** -0.5, self.step_num * self.warmup_step ** -1.5)
-        self.optimizer.defaults['lr'] = new_lr
+        new_lr = self.init_lr * embedding_dim ** -0.5 * min(self.step_num ** -0.5, self.step_num * self.warmup_step ** -1.5)
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = new_lr
 
     def step(self):
         self.step_num += 1
