6c3eb8b550dcaa9ba875a193f6dbe866c23b9ed1
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-06 12:53:06 +0800

    Update bleu.py

--- evaluation/bleu.py
+++ evaluation/bleu.py
@@ -1,21 +1,20 @@
 from evaluation.beam_search import *
-from data.dataset import CorpusDataset
-
-dataset = CorpusDataset('../data/corpus/train_en', '../data/corpus/train_cn', '../data/vocab.pkl', model_config)
 
 
 def bleu(src, targets):
     pass
 
 
-def translate(sentences):
+def translate(model, converter, sentences):
     """
+    :param model: Transformer
+    :param converter: TokenSentenceConverter
     :param sentences: 一个英文句子列表
     :return: results: 中文句子列表
     """
-    token = torch.nn.utils.rnn.pad_sequence([dataset.sentence2token(sen, 'en').to(device)
+    token = torch.nn.utils.rnn.pad_sequence([converter.sentence2token(sen, 'en').to(device)
                                              for sen in sentences],
                                             batch_first=True)
-    decode = beam_search(token, max_length=100, num_beams=3)
-    results = [dataset.token2sentence(sen, 'cn') for sen in decode]
+    decode = beam_search(model, token, max_length=100, num_beams=3)
+    results = [converter.token2sentence(sen, 'cn') for sen in decode]
     return results
