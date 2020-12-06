c787afaca9c5e3b079a694b181ea2867318cd911
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-07 00:58:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 import tqdm
 from transformer import Transformer, Config
 from data.dataset import TokenSentenceConverter, CorpusDataset, sentence_collate_fn
+from warm_up import WarmUpLr
 from config import *
 
 
@@ -12,6 +13,7 @@ class Trainer(object):
                  lr=1e-4, model_state_dict=None,
                  optimizer_state_dict=None, use_cuda=True, seed=10):
         self.seed = seed
+        # 初始化之前固定种子
         torch.manual_seed(seed)
         self.device = torch.device('cuda:0' if use_cuda and torch.cuda.is_available() else 'cpu')
         self.model = model(Config(model_config))
@@ -20,29 +22,18 @@ class Trainer(object):
             self.model.load_state_dict(model_state_dict)
         self.model.to(self.device)
         self.train_loader = DataLoader(train_set, batch_size=32, shuffle=True, collate_fn=sentence_collate_fn)
-        self.optimizer = Adam(self.model.parameters(), lr=lr)
+        self.optimizer = WarmUpLr(Adam(self.model.parameters(), lr=lr, betas=(0.9, 0.98), eps=1e-09), warmup_step=1000)
         if optimizer_state_dict is not None:
-            self.optimizer.load_state_dict(optimizer_state_dict)
+            self.optimizer.optimizer.load_state_dict(optimizer_state_dict)
         self.loss_fcn = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
-    @torch.no_grad()
-    def calculate_loss(self, dataloader):
-        loss = 0
-        for src, tgt in tqdm.tqdm(dataloader, desc='calculate loss', total=len(dataloader)):
-            src = src.to(self.device)
-            tgt = tgt.to(self.device)
-            output, label = self.model(src, tgt)
-            loss += self.loss_fcn(output.transpose(1, 2), label).item() * src.shape[0]
-        return loss / len(dataloader.dataset)
-
     def train(self, epochs):
-        torch.manual_seed(self.seed)
         for epoch in range(epochs):
             for i, (src, tgt) in tqdm.tqdm(enumerate(self.train_loader), desc='Epoch_%s' % epoch, total=len(self.train_loader)):
                 src = src.to(self.device)
                 tgt = tgt.to(self.device)
-                output, label = self.model(src, tgt)
-                loss = self.loss_fcn(output.transpose(1, 2), label)
+                output, gold = self.model(src, tgt)
+                loss = self.loss_fcn(output.transpose(1, 2), gold)
                 print('Loss: ', loss.item())
                 loss.backward()
                 self.optimizer.step()
@@ -56,11 +47,11 @@ if __name__ == '__main__':
         model=Transformer,
         model_config=model_config,
         train_set=train_set,
-        lr=1e-4,
-        model_state_dict=torch.load('model/2epoch/transformer.pkl'),
-        optimizer_state_dict=torch.load('model/2epoch/optimizer.pkl'),
+        lr=5e-4,
+        # model_state_dict=torch.load('model/3epoch/transformer.pkl'),
+        # optimizer_state_dict=torch.load('model/3epoch/optimizer.pkl'),
         seed=10
     )
     trainer.train(1)
-    torch.save(trainer.model.state_dict(), 'model/3epoch/transformer.pkl')
-    torch.save(trainer.optimizer.state_dict(), 'model/3epoch/optimizer.pkl')
+    torch.save(trainer.model.state_dict(), 'model/1epoch/transformer.pkl')
+    torch.save(trainer.optimizer.optimizer.state_dict(), 'model/1epoch/optimizer.pkl')
