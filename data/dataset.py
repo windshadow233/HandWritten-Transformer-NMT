b51e0400ad918853b5fe69d3a56b7b2701c2dd35
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-06 12:52:54 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -14,38 +14,23 @@ def sentence_collate_fn(one_batch):
     return src, tgt
 
 
-class CorpusDataset(Dataset):
-    def __init__(self, src_file, tgt_file, vocab_dict, model_config):
-        self.max_len = model_config.getint('max_seq_len')
+class TokenSentenceConverter(object):
+    def __init__(self, vocab_dict):
         with open(vocab_dict, 'rb') as f:
             vocab_dict = pickle.load(f).get('vocab_dict')
         self.src_char2idx = vocab_dict.get('src_char2idx')
         self.src_idx2char = vocab_dict.get('src_idx2char')
         self.tgt_char2idx = vocab_dict.get('tgt_char2idx')
         self.tgt_idx2char = vocab_dict.get('tgt_idx2char')
-        with open(src_file, 'r', encoding='utf-8') as f:
-            self.src_sentences = f.readlines()
-        with open(tgt_file, 'r', encoding='utf-8') as f:
-            self.tgt_sentences = f.readlines()
-
-    def __len__(self):
-        return len(self.src_sentences)
-
-    def __getitem__(self, item):
-        src_sen = self.src_sentences[item].strip()
-        tgt_sen = self.tgt_sentences[item].strip()
-        src_token = self.sentence2token(src_sen, 'en')
-        tgt_token = self.sentence2token(tgt_sen, 'cn')
-        return src_token, tgt_token
 
     def sentence2token(self, sentence, lang='en'):
         assert lang in {'en', 'cn'}
         char2idx = self.src_char2idx if lang == 'en' else self.tgt_char2idx
         if lang == 'en':
             sentence = [normalize_string(s.strip()) for s in nltk.word_tokenize(sentence)]
-            sentence = sentence[:self.max_len]
+            sentence = sentence[:max_seq_len]
         else:
-            sentence = sentence[:self.max_len - 2]
+            sentence = sentence[:max_seq_len - 2]
             sentence = ['<sos>'] + list(sentence) + ['<eos>']
         return torch.tensor([char2idx.get(word, char2idx['<unk>']) for word in sentence], dtype=torch.long)
 
@@ -58,3 +43,22 @@ class CorpusDataset(Dataset):
         if lang == 'en':
             return ' '.join(sentence)
         return ''.join(sentence)
+
+
+class CorpusDataset(Dataset):
+    def __init__(self, src_file, tgt_file, converter: TokenSentenceConverter):
+        with open(src_file, 'r', encoding='utf-8') as f:
+            self.src_sentences = f.readlines()
+        with open(tgt_file, 'r', encoding='utf-8') as f:
+            self.tgt_sentences = f.readlines()
+        self.converter = converter
+
+    def __len__(self):
+        return len(self.src_sentences)
+
+    def __getitem__(self, item):
+        src_sen = self.src_sentences[item].strip()
+        tgt_sen = self.tgt_sentences[item].strip()
+        src_token = self.converter.sentence2token(src_sen, 'en')
+        tgt_token = self.converter.sentence2token(tgt_sen, 'cn')
+        return src_token, tgt_token
