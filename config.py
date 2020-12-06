fc059642d903b338e02dbcbccf279bd209a26e81
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-07 00:58:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ parser = ConfigParser()
 parser.read('./config.ini', encoding='utf-8')
 model_config = parser['model_config']
 max_seq_len = model_config.getint('max_seq_len')
+embedding_dim = model_config.getint('embedding_dim')
 pad_idx = model_config.getint('pad_idx')
 sos_idx = model_config.getint('sos_idx')
 eos_idx = model_config.getint('eos_idx')
