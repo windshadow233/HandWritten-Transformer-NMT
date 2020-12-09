f36a0fcfdf4c17372908c7489c148303fc7b10a9
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-10 01:29:21 +0800

    Update translate.py

--- evaluation/translate.py
+++ evaluation/translate.py
@@ -1,4 +1,5 @@
 from evaluation.beam_search import *
+from data.utils import full_width2half_width
 
 
 @torch.no_grad()
@@ -9,6 +10,7 @@ def translate_batch(model, converter, sentences):
     :param sentences: 一个英文句子列表
     :return: results: 中文句子列表
     """
+    sentences = [full_width2half_width(s) for s in sentences]
     sentences = [s + '.' if not s.endswith(('.', '!', '?')) else s for s in sentences]
     token = torch.nn.utils.rnn.pad_sequence([converter.sentence2token(sen, 'en').to(device)
                                              for sen in sentences],
