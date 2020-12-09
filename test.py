9bac04d379260ec533c90120924f606b670b69cc
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-09 14:35:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ from transformer import Transformer, Config
 from data.dataset import CorpusDataset, TokenSentenceConverter, sentence_collate_fn
 from nltk.translate.bleu_score import sentence_bleu
 from nltk import bleu
+import tqdm
 import time
-from evaluation.translate import translate
+from evaluation.translate import translate_batch
 from config import *
 
 model = Transformer(Config(model_config))
@@ -14,13 +15,16 @@ model.cuda()
 model.eval()
 converter = TokenSentenceConverter('data/vocab.pkl')
 dataset = CorpusDataset('data/corpus/test_en', 'data/corpus/test_cn', converter)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=sentence_collate_fn)
-translate_one_sen = lambda x: translate(model, converter, [x])[0]
-with torch.no_grad():
-    dataloader = iter(dataloader)
-    src, tgt = next(dataloader)
-    src = [converter.token2sentence(sen, 'en') for sen in src]
-    tgt = [converter.token2sentence(sen, 'cn').replace('<sos>', '').replace('<eos>', '') for sen in tgt]
-    result = translate(model, converter, src)
-    for r, t in zip(result, tgt):
-        print(bleu([list(t)], list(r)))
+dataloader = DataLoader(dataset, batch_size=20, shuffle=True, collate_fn=sentence_collate_fn)
+translate = lambda x: list(translate_batch(model, converter, [x]).items())[0][1]
+bleu_ = 0
+dataloader = iter(dataloader)
+# with torch.no_grad():
+#     for _ in tqdm.tqdm(range(50)):
+#         src, tgt = next(dataloader)
+#         src = [converter.token2sentence(sen, 'en') for sen in src]
+#         tgt = [converter.token2sentence(sen[1: -1], 'cn') for sen in tgt]
+#         result = translate(model, converter, src)
+#         for r, t in zip(result, tgt):
+#             bleu_ += bleu([list(t)], list(r))
+# print(bleu_ / 1000)
