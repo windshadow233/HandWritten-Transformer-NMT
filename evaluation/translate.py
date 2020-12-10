3958846a1b226b39d979c5632f1df06cdade8f2e
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-10 21:18:52 +0800

    Update translate.py

--- evaluation/translate.py
+++ evaluation/translate.py
@@ -4,19 +4,26 @@ from data.utils import full_width2half_width
 
 
 @torch.no_grad()
-def translate_batch(model, converter, sentences):
+def translate_batch(model, converter, src, tgt=None):
     """
     :param model: Transformer
     :param converter: TokenSentenceConverter
-    :param sentences: 一个英文句子列表
-    :return: results: 中文句子列表
+    :param src: 一个英文句子列表
+    :param tgt: target
+    :return: predicts: 中文句子列表
     """
-    sentences = [full_width2half_width(s) for s in sentences]
-    sentences = [s + '.' if not s.endswith(('.', '!', '?')) else s for s in sentences]
+    src = [full_width2half_width(s) for s in src]
+    src = [s + '.' if not s.endswith(('.', '!', '?')) else s for s in src]
     token = torch.nn.utils.rnn.pad_sequence([converter.sentence2token(sen, 'en').to(device)
-                                             for sen in sentences],
+                                             for sen in src],
                                             batch_first=True)
     decode = beam_search(model, token, num_beams=5)
-    results = [converter.token2sentence(sen, 'cn').replace('<sos>', '').replace('<eos>', '') for sen in decode]
-    results = dict(zip(sentences, results))
-    return results
+    predicts = [converter.token2sentence(sen, 'cn').replace('<sos>', '').replace('<eos>', '') for sen in decode]
+    if tgt is not None:
+        for s, p, t in zip(src, predicts, tgt):
+            print(f'Src  | {s}\nTgt  | {t}\nPred | {p}\n')
+    else:
+        for s, p in zip(src, predicts):
+            print(f'Src  | {s}\nPred | {p}\n')
+    predicts = dict(zip(src, predicts))
+    return predicts
