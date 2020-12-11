770fca861c18b0885b2a58669418558194a4d2e7
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-11 20:51:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-import torch
 from torch.utils.data import DataLoader
 from transformer import Transformer, Config
 from data.dataset import CorpusDataset, TokenSentenceConverter
 import tqdm
-import time
 from evaluation.translate import translate_batch
 from config import *
 
@@ -14,16 +12,30 @@ model.eval()
 batch_size = 25
 converter = TokenSentenceConverter('data/vocab.pkl')
 dataset = CorpusDataset('data/corpus/test_en', 'data/corpus/test_cn', converter, to_token=False)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False,
                         collate_fn=lambda x: ([s[0] for s in x], [s[1] for s in x]))
-translate = lambda x: translate_batch(model, converter, [x])
+translate = lambda x: translate_batch(model, converter, [x])[1]
 bleu_ = 0
 dataloader = iter(dataloader)
-batches = 5
+batches = 2
+translate_result = 'Top 50 Results:\n\n'
 with torch.no_grad(), tqdm.tqdm(range(batches)) as t:
     for _ in t:
         src, tgt = next(dataloader)
-        results = translate_batch(model, converter, src, tgt)
-        for *_, b in results:
-            bleu_ += b
-print(bleu_ / batch_size / batches)
+        result, s = translate_batch(model, converter, src, tgt)
+        translate_result += s
+        bleu_ += sum(map(lambda x: x[-1], result))
+translate_result += f'Top 50 BLEU: {bleu_ / batches / batch_size}'
+with open('data/results/test_results.txt', 'w', encoding='utf-8') as f:
+    f.write(translate_result)
+
+
+batch_size = 32
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False,
+                        collate_fn=lambda x: ([s[0] for s in x], [s[1] for s in x]))
+bleu_ = 0
+for src, tgt in tqdm.tqdm(dataloader):
+    result, s = translate_batch(model, converter, src, tgt)
+    bleu_ += sum(map(lambda x: x[-1], result))
+with open('data/results/test_results.txt', 'a+', encoding='utf-8') as f:
+    f.write(f'\nTotal BLEU: {bleu_ / len(dataloader.dataset)}')
